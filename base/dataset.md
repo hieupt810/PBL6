04556c52eabd38712d8b9fa79de4686b63f9bedd
Pham Trung Hieu <trunghieupham03@gmail.com>
2024-10-27 20:40:21 +0700

    feat: update data in dataset.md

--- base/dataset.md
+++ base/dataset.md
@@ -1,41 +1,49 @@
 # Dataset Categories
 
-## Consumer Electronics
+The numbers in parentheses represent the quantity of images used for training, testing, and validation, respectively.
 
-1. Fan
-2. Keyboard
-3. Monitor
-4. Mouse
-5. PC
-6. Phone
-7. Television
-8. Speaker
-9. Smartwatch
-10. Camera
-11. Printer
-12. Microwave
+### Consumer Electronics
 
-## Home
+| Product name | Training | Validation | Testing |
+| ------------ | -------: | ---------: | ------: |
+| Fan          |       10 |          3 |       0 |
+| Keyboard     |       10 |          3 |       0 |
+| Monitor      |       10 |          3 |       0 |
+| Mouse        |       10 |          3 |       0 |
+| PC           |       10 |          3 |       0 |
+| Phone        |       10 |          3 |       5 |
+| Speaker      |       10 |          3 |       0 |
+| Television   |       10 |          3 |       0 |
+| Smartwatch   |        0 |          0 |       0 |
+| Camera       |        0 |          0 |       0 |
+| Printer      |        0 |          0 |       0 |
+| Microwave    |        0 |          0 |       0 |
 
-1. Chair
-2. Chopsticks
-3. Knife
-4. Pillow
-5. Pot
-6. Scissors
-7. Spoon
-8. Table
-9. Wardrobe
+### Fashion
 
-## Fashion
+| Product name | Training | Validation | Testing |
+| ------------ | -------: | ---------: | ------: |
+| Backpack     |       10 |          3 |       5 |
+| Eyewear      |       10 |          3 |       5 |
+| Handbag      |       10 |          3 |       5 |
+| Hoodie       |       10 |          3 |       5 |
+| Jacket       |       10 |          3 |       5 |
+| Shirt        |       10 |          3 |       5 |
+| Shoes        |       10 |          3 |       5 |
+| Wallet       |       10 |          3 |       5 |
+| Accessories  |        0 |          0 |       0 |
+| Bag          |        0 |          0 |       0 |
 
-1. Shoes
-2. Accessories
-3. Accessories
-4. T-Shirt
-5. Hoodie
-6. Jacket
-7. Bag
-8. Wallet
-9. Backpack
-10. Handbag
+### Home
+
+| Product name | Training | Validation | Testing |
+| ------------ | -------: | ---------: | ------: |
+| Chair        |       10 |          3 |       0 |
+| Chopsticks   |       10 |          3 |       0 |
+| Knife        |       10 |          3 |       0 |
+| Pillow       |       10 |          3 |       0 |
+| Pot          |       10 |          3 |       0 |
+| Scissors     |       10 |          3 |       0 |
+| Spoon        |       10 |          3 |       5 |
+| Table        |       10 |          3 |       5 |
+| Wardrobe     |        3 |          3 |       0 |
--- base/train.py
+++ base/train.py
@@ -1,12 +1,12 @@
 import os
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
+from tqdm import tqdm
 from transformers import ViTForImageClassification, ViTImageProcessor
 
 
@@ -20,7 +20,8 @@ def load_datasets(transform: transforms.Compose, root: str = "/", batch_size: in
     return load_dataset("train", shuffle=True), load_dataset("validate")
 
 
-def load_model(name: str):
+def load_model(name: str, checkpoint_dir: str = None):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ViTForImageClassification.from_pretrained(name)
     processor = ViTImageProcessor.from_pretrained(name)
     data_augmentation = transforms.Compose(
@@ -41,14 +42,19 @@ def load_model(name: str):
             transforms.Normalize(mean=processor.image_mean, std=processor.image_std),
         ]
     )
-    return model, transform
+    if checkpoint_dir:
+        model.load_state_dict(
+            torch.load(checkpoint_dir, map_location=device, weights_only=True)
+        )
+
+    return model.to(device), transform, device
 
 
 root = os.path.dirname(os.path.abspath(__file__))
 checkpoints_dir = os.path.join(root, "checkpoints")
 os.makedirs(checkpoints_dir, exist_ok=True)
 
-model, transform = load_model("google/vit-base-patch16-224")
+model, transform, device = load_model("google/vit-base-patch16-224")
 train_loader, validate_loader = load_datasets(root=root, transform=transform)
 
 epochs = 10
@@ -56,17 +62,14 @@ loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
 losses, accuracies = [], []
 for epoch in range(epochs):
-    print(f"Epoch {epoch + 1}/{epochs}")
+    print(f"- Epoch {epoch + 1}/{epochs}")
 
     # Training
     model.train()
     running_loss = 0.0
-    for images, labels in train_loader:
+    for images, labels in tqdm(train_loader, desc="Training"):
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images).logits
@@ -75,42 +78,36 @@ for epoch in range(epochs):
         optimizer.step()
         running_loss += loss.item()
 
-    print("- Loss:", format(running_loss / len(train_loader), ".4f"))
-
     # Validation
     model.eval()
     correct, total = 0, 0
     with torch.no_grad():
-        for images, labels in validate_loader:
+        for images, labels in tqdm(validate_loader, desc="Validating"):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images).logits
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print(f"- Validation accuracy: {100 * correct / total:.2f}%")
+    print(
+        f"Loss: {running_loss / len(train_loader):.4f} | Accuracy: {100 * correct / total:.2f}%"
+    )
     losses.append(running_loss / len(train_loader))
     accuracies.append(100 * correct / total)
     scheduler.step()
 
-    # Save the model every 5 epochs
-    if (epoch + 1) % 5 == 0:
+    if losses[-1] < 1.0 and accuracies[-1] >= 90:
         torch.save(
             model.state_dict(),
             os.path.join(checkpoints_dir, f"checkpoint{epoch + 1}.pth"),
         )
 
-# Plot the training loss
 plt.plot(losses, label="Training loss")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-plt.xticks(np.arange(0, epochs, step=1))
 plt.show()
 
-# Plot the validation accuracy
 plt.plot(accuracies, label="Validation accuracy")
 plt.xlabel("Epoch")
 plt.ylabel("Accuracy")
-plt.xticks(np.arange(0, epochs, step=1))
-plt.yticks(np.arange(0, 100, step=10))
 plt.show()
